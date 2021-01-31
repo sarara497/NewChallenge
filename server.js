66050432e6480d617668c124f0f415bae581e5ff
sarara497 <sara19972020@outlook.com>
2021-01-31 06:33:42 +0200

    Deploy

--- server.js
+++ server.js
@@ -11,13 +11,13 @@ app.use(cors())
 
 app.use('/Items' , require('./Routes/Items'))
 
-// if(process.env.NODE_ENV === 'production'){ 
-//   app.use(express.static('snack-vending-machine/build'))
+if(process.env.NODE_ENV === 'production'){ 
+  app.use(express.static('snack-vending-machine/build'))
 
-//   app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "snack-vending-machine", "build", "index.html"))
-//     })
-// }
+  app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "snack-vending-machine", "build", "index.html"))
+    })
+}
 
 app.use((req,res) => {
     res.send('Welcome to Backend *_^')
